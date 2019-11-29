3b8ceb7d14bb578c76e56611aa9b7b8a0bd79707
frannyfra <francescadelaurentis1987@gmail.com>
2019-11-29 15:23:27 +0000

    add profile pic to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,27 @@
 import React from 'react';
 // import { BrowserRouter, Route, Link } from 'react-router-dom';
 import logo from '../Assets/Logo.svg';
+import profilePic from '../Assets/Francesca.jpeg';
 import "./NavBar.css";
+// import { url } from 'inspector';
 // import about from './About';
 // import projects from './Projects';
 // import introduction from './Introduction';
 
-class NavBar extends React.Component {
+class NavBar extends React.Component{
 	render() { 
 		return (
-        <div className="navigation">
-        <img src={logo} className="logo" alt="full name initials" />
+      <div className="navigation">
 
-
-        
-        
+        <div className="text-center">
+          <img src={logo} className="logo" alt="full name initials" />
+          <div className="author-img"><img src={profilePic} /></div>
+          <h3 id="full-name">Francesca De Laurentis</h3>
+          <span className="email">francescadelaurentis1987@gmail.com</span>
+        </div>
+    
         <div className="navigation-sub">
           <ul id="nav">
-            <span>francescadelaurentis1987@gmail.com</span>
             <li><a href="introduction">Introduction</a></li>
             <li><a href="about">About</a></li>
             <li><a href="projects">Projects</a></li>
