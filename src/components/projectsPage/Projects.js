2c329236ee14273a532a5bf88c60c1b4c2521dc2
frannyfra <francescadelaurentis1987@gmail.com>
2020-01-04 18:02:21 +0000

    styling project page with overlay

--- src/components/projectsPage/Projects.js
+++ src/components/projectsPage/Projects.js
@@ -19,23 +19,78 @@ const Projects = () => {
       </Link>
 
       <section className="project-page">
-        <h2 className="projects-title">Projects</h2>
+        {/* <h2 className="projects-title">Projects</h2> */}
         <hr className="line"/>
         <div className="project">
+
           <article className="project-item">
+            {/* <h3>
+              My Body Back
+            </h3> */}
             <img 
             className="project-image"
             src={require("../../Assets/myBodyBack.png")}
             alt="project"
             ></img>
+            {/* <h3>
+              My Body Back
+            </h3>
+            <p>
+              React | Netlify | Lambda | Jest | Enzyme | Airtable | React-Swipeable
+            </p> */}
+            <div className="overlay">
+              <div className="text">
+                <p>
+                A MVP built as part of the Founders&Coders' Tech for Better programme
 
+                <br>
+                </br>
+                <br></br>
+                The app aims to provide support for people with experience of sexual assault. The app will encourage more people to go for a routine cervical screening
+                </p>
+                <p>
+                  Role: <br></br>UX/UI & developer<br></br>
+                </p>
+                <a href="https://github.com/fac-17/My-Body-Back" target="_blank" className="github-link">
+                  Go to Github
+                </a>
+              </div>
+            </div>
           </article>
+
           <article className="project-item">
+            {/* <h3>
+              NomNom
+            </h3> */}
             <img 
             className="project-image"
             src={require("../../Assets/NomNom.png")}
             alt="project"
             ></img>
+            {/* <h3>
+              NomNom
+            </h3> */}
+            <p>
+
+            </p>
+            <div className="overlay">
+              <div className="text">
+                <p>
+                A MVP built as part of the Founders&Coders' student project
+
+                <br>
+                </br>
+                <br></br>
+                Build a planning meal app that suggest a week-long meal plans with common ingredients and provides healthy options in order to reduce food waste.
+                </p>
+                <p>
+                  Role: <br></br>DevOps & developer<br></br>
+                </p>
+                <a href="https://github.com/fac-17/NomNom" target="_blank" className="github-link">
+                  Go to Github
+                </a>
+              </div>
+            </div>
 
           </article>
 
@@ -45,9 +100,24 @@ const Projects = () => {
             src={require("../../Assets/githubPage.png")}
             alt="project"
             ></img>
+            <div className="overlay">
+              <div className="text">
+                <p>
+                  Visit my Github profile to find out about more projects&repositories
+                <br>
+                </br>
+                <br></br>
+                Weekly projects realised to practise the skill the learnt at Founders&Coders
+                </p>
+                <a href="https://github.com/frannyfra" target="_blank" className="github-link">
+                  Go to Github
+                </a>
+              </div>
+            </div>
 
           </article>
         </div>
+        <hr className="line"/>
       </section>
      
       <Link to={{ pathname: "/contact-me" }} className="arrow-down-router">
